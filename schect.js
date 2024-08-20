91803e294d0b634b9ad2a7cc7b54d13a37b8ac8d
ar282 <arthur.callegaro@escola.pr.gov.br>
2024-08-20 15:07:22 -0300

    Update schect.js

--- schect.js
+++ schect.js
@@ -1,1 +1,13 @@
+function setup() {
+  createCanvas(700, 700);
+  background("black")
+}
 
+function draw() {
+  stroke("rgb(255,255,));
+  fill("rgb(253,253,253)");
+  
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
